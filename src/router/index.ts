97cdae6a344a9df9984fe6f27c6b44ca65f5bac6
Raúl <ralo.dev@pm.me>
2024-01-10 15:55:10 -0600

    Update index.ts
    Router changed from hash to html

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,46 +1,61 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import LandingView from '../views/HomeView.vue'
-import AboutView from '../views/AboutView.vue'
 import SgipView from '@/views/projects/SgipView.vue'
-import EasterEgg from '@/views/EasterEgg.vue'
 
 const router = createRouter({
-  history: createWebHashHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
       name: 'Home',
       component: LandingView
     },
-    {
-      path: '/about',
-      name: 'About',
-      component: AboutView
-    },
-    {
-      path: '/easter-egg',
-      name: 'EasterEgg',
-      component: EasterEgg
-    },
+    // {
+    //   path: '/es/',
+    //   name: 'Home',
+    //   component: LandingView
+    // },
     {
       path: '/projects/SGIP',
       name: 'SGIP',
       component: SgipView
     },
+    // {
+    //   path: '/es/projects/SGIP',
+    //   name: 'SGIP',
+    //   component: SgipView
+    // },
+    {
+      path: '/404',
+      name: '404',
+      component: LandingView
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      redirect: '/404'
+    }
   ],
-  scrollBehavior(to) {
+  scrollBehavior(to, from, savedPosition) {
+    // if coming from SGIP page, scroll to project section
+    if (from.name === 'SGIP' && savedPosition) {
+      return {
+        el: '#projects',
+        behavior: 'smooth'
+      }
+    }
+    // if link has anchor, scroll to anchor
     if (to.hash) {
       return {
         el: to.hash,
         behavior: 'smooth'
       }
-    } else {
-      return {
-        top: 0,
-        behavior: 'smooth'
-      }
     }
-  },
+    // else scroll to top
+    return {
+      top: 0,
+      behavior: 'smooth'
+    }
+  }
 })
 
 export default router
