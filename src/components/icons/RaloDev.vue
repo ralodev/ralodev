c12de62c56ba90ed223daf06cd61928adff468c2
Raúl <ralo.dev@pm.me>
2024-01-10 15:55:38 -0600

    Create RaloDev.vue
    SVG dinamic logo

---
+++ src/components/icons/RaloDev.vue
@@ -0,0 +1,221 @@
+<template>
+  <svg
+    viewBox="761.672 800.1868 3447.7921 3427.1053"
+    width="3447.7921"
+    height="3427.1053"
+    xmlns="http://www.w3.org/2000/svg"
+    class="group"
+  >
+    <defs>
+      <linearGradient
+        x1="2729.6699"
+        y1="-273.36401"
+        x2="1175.52"
+        y2="1001.84"
+        gradientUnits="userSpaceOnUse"
+        gradientTransform="matrix(7.5,0,0,-7.5,9150,24367.5)"
+        spreadMethod="pad"
+        id="linearGradient28"
+      >
+        <stop
+          style="stop-opacity: 1; stop-color: #447ffa"
+          offset="0"
+          id="stop22"
+        />
+        <stop
+          style="stop-opacity: 1; stop-color: rgb(60, 111, 218)"
+          offset="0.513"
+          id="stop24"
+        />
+        <stop
+          style="stop-opacity: 1; stop-color: rgb(27, 51, 101)"
+          offset="1"
+          id="stop26"
+        />
+      </linearGradient>
+      <linearGradient
+        x1="-597.875"
+        y1="-652.01001"
+        x2="2610.3701"
+        y2="2556.23"
+        gradientUnits="userSpaceOnUse"
+        gradientTransform="matrix(7.5,0,0,-7.5,9150,24367.5)"
+        spreadMethod="pad"
+        id="linearGradient46"
+      >
+        <stop
+          style="stop-opacity: 1; stop-color: #af39cc"
+          offset="0"
+          id="stop40"
+        />
+        <stop
+          style="stop-opacity: 1; stop-color: rgb(63, 117, 230)"
+          offset="0.513"
+          id="stop42"
+        />
+        <stop
+          style="stop-opacity: 1; stop-color: rgb(44, 83, 165)"
+          offset="1"
+          id="stop44"
+        />
+      </linearGradient>
+      <linearGradient
+        x1="-310.19699"
+        y1="890.75598"
+        x2="1505.71"
+        y2="2706.6699"
+        gradientUnits="userSpaceOnUse"
+        gradientTransform="matrix(7.5,0,0,-7.5,9150,24367.5)"
+        spreadMethod="pad"
+        id="linearGradient64"
+      >
+        <stop
+          style="stop-opacity: 1; stop-color: #af39cc"
+          offset="0"
+          id="stop58"
+        />
+        <stop
+          style="stop-opacity: 1; stop-color: rgb(63, 117, 230)"
+          offset="0.513"
+          id="stop60"
+        />
+        <stop
+          style="stop-opacity: 1; stop-color: rgb(44, 83, 165)"
+          offset="1"
+          id="stop62"
+        />
+      </linearGradient>
+      <linearGradient
+        x1="-259.07901"
+        y1="1758.99"
+        x2="637.76703"
+        y2="2655.8301"
+        gradientUnits="userSpaceOnUse"
+        gradientTransform="matrix(7.5,0,0,-7.5,9150,24367.5)"
+        spreadMethod="pad"
+        id="linearGradient82"
+      >
+        <stop
+          style="stop-opacity: 1; stop-color: #af39cc"
+          offset="0"
+          id="stop76"
+        />
+        <stop
+          style="stop-opacity: 1; stop-color: rgb(63, 117, 230)"
+          offset="0.513"
+          id="stop78"
+        />
+        <stop
+          style="stop-opacity: 1; stop-color: rgb(44, 83, 165)"
+          offset="1"
+          id="stop80"
+        />
+      </linearGradient>
+      <linearGradient
+        x1="2092.0701"
+        y1="-34.264301"
+        x2="1055.26"
+        y2="604.047"
+        gradientUnits="userSpaceOnUse"
+        gradientTransform="matrix(7.5,0,0,-7.5,9150,24367.5)"
+        spreadMethod="pad"
+        id="linearGradient100"
+      >
+        <stop
+          style="stop-opacity: 1; stop-color: #447ffa"
+          offset="0"
+          id="stop94"
+        />
+        <stop
+          style="stop-opacity: 1; stop-color: rgb(59, 111, 217)"
+          offset="0.513"
+          id="stop96"
+        />
+        <stop
+          style="stop-opacity: 1; stop-color: rgb(33, 62, 123)"
+          offset="1"
+          id="stop98"
+        />
+      </linearGradient>
+    </defs>
+    <g
+      id="g8"
+      transform="matrix(1.3333332538604734, 0, 0, -1.3333332538604734, 1.4210854715202004e-14, 5000)"
+    >
+      <g id="g10" transform="scale(0.1)">
+        <g id="g14">
+          <g id="g16" clip-path="url(#clipPath20)">
+            <path
+              d="M 22150.7,31498.6 H 5712.54 L 9299.02,27912.1 H 22150.7 c 1607,-378.3 2907.2,-1099.9 3900.7,-2164.6 889.2,-916.4 1501.3,-2040.3 1836.7,-3371.6 272.2,-1473.1 281.2,-2617.7 26.9,-3433.9 -690.7,-2635.7 -2313.3,-4427.7 -4867.7,-5375.9 l 2689.9,-2689.9 c 174.6,66.7 639.4,361.5 1394.2,884.3 590.6,440.8 1132.4,935.1 1625.3,1483 742.5,825.3 1346.4,1741.8 1811.5,2749.7 934,2051.1 1215.6,4219.5 844.6,6505.1 -542.9,2523.9 -1584,4507.8 -3123.4,5951.6 -1913.8,1755.2 -3960.1,2771.4 -6138.7,3048.7 z"
+              style="
+                fill: url(#linearGradient28);
+                fill-opacity: 1;
+                fill-rule: nonzero;
+                stroke: none;
+              "
+              id="outerCurve"
+            />
+          </g>
+        </g>
+        <g id="g32">
+          <g id="g34" clip-path="url(#clipPath38)">
+            <path
+              d="M 5712.54,31498.6 V 26417.7 L 26334.9,5795.31 l 5080.9,0.04 z"
+              style="
+                fill: url(#linearGradient46);
+                fill-opacity: 1;
+                fill-rule: nonzero;
+                stroke: none;
+              "
+              id="topLine"
+            />
+          </g>
+        </g>
+        <g id="g50">
+          <g id="g52" clip-path="url(#clipPath56)" class="">
+            <path
+              d="m 5712.54,22831.3 v -0.1 C 11391.1,17152.6 17069.8,11474 22748.4,5795.35 l -5080.9,-0.04 C 13682.5,9780.35 9697.54,13765.4 5712.54,17750.4 v 5080.9"
+              style="
+                fill: url(#linearGradient64);
+                fill-opacity: 1;
+                fill-rule: nonzero;
+                stroke: none;
+              "
+              id="middleLine"
+              class="group-hover:opacity-0 duration-300 group-hover:-translate-x-[-250%] ease-in-out"
+            />
+          </g>
+        </g>
+        <g id="g68">
+          <g id="g70" clip-path="url(#clipPath74)">
+            <path
+              d="M 5712.54,14462.7 14379.9,5795.35 c -1693.6,0 -3387.2,0 -5080.88,0 -1195.5,1195.51 -2390.97,2391.02 -3586.48,3586.53 v 5080.82"
+              style="
+                fill: url(#linearGradient82);
+                fill-opacity: 1;
+                fill-rule: nonzero;
+                stroke: none;
+              "
+              id="bottomLine"
+            />
+          </g>
+        </g>
+        <g id="g86">
+          <g id="g88" clip-path="url(#clipPath92)">
+            <path
+              d="m 11690,25521.1 h 9564 c 1230.9,-297 2186.4,-891.8 2866.5,-1784.2 663.9,-863.7 1006.7,-1856.5 1028.4,-2978.1 -0.5,-587.8 -102.1,-1157.7 -304.8,-1709.9 -263.9,-627.5 -495.9,-1051.4 -695.7,-1271.6 -747.7,-951.9 -1712.5,-1558.6 -2894.4,-1820.2 l -3287.6,3287.6 h 2988.8 c 97.4,39.6 189.4,89.3 276,149.1 361.6,257.5 568.9,606.7 621.9,1047.5 42.2,444.9 -98.6,838.1 -422.4,1179.7 -148.9,142.9 -307.4,247.5 -475.5,313.6 H 15276.5 L 11690,25521.1"
+              style="
+                fill: url(#linearGradient100);
+                fill-opacity: 1;
+                fill-rule: nonzero;
+                stroke: none;
+              "
+              id="innerCurve"
+              class="group-hover:opacity-0 duration-300 group-hover:-translate-x-[-250%] ease-in-out"
+            />
+          </g>
+        </g>
+      </g>
+    </g>
+  </svg>
+</template>
