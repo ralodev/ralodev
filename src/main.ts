72aca0232863fcfa9f814ec0677c34bd62c7e7e8
Raúl <ralo.dev@pm.me>
2024-01-14 21:42:12 -0600

    Removed font import

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,6 @@ import App from './App.vue'
 import router from './router'
 // Supports weights 100-900
 import '@fontsource-variable/onest';
-// Supports weights 400-700
-import '@fontsource-variable/expletus-sans';
 // Supports weights 200-900
 import '@fontsource-variable/source-code-pro';
 
