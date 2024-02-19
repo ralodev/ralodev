016f445b6c428f6e182aca1b8d63f7b29d71ec44
Raúl <ralo.dev@pm.me>
2024-02-18 20:36:10 -0600

    Update FooterBar.vue with new styles and source code link

--- src/components/FooterBar.vue
+++ src/components/FooterBar.vue
@@ -1,66 +1,29 @@
 <template>
   <footer
-    class="relative z-10 mt-5 overflow-hidden bg-gray-300 pb-10 pt-[120px] dark:bg-gradient-to-b dark:from-neutral-950 dark:to-neutral-800 dark:text-dtext1"
+    class="relative z-10 mx-auto mt-5 flex max-w-[1000px] justify-between overflow-hidden pb-4 pt-[120px] text-sm dark:text-neutral-300"
   >
-    <div class="container mx-auto max-w-[1200px]">
-      <div class="flex flex-wrap">
-        <div class="w-full px-4">
-          <div class="flex w-full gap-3">
-            <div class="">
-              <RouterLink
-                class="group relative"
-                :to="{ name: 'Home' }"
-                offset="{0}"
-                duration="{500}"
-                isDynamic="{true}"
-              >
-                <img
-                  src="/r_plain.svg"
-                  alt="logo"
-                  class="max-w-[64px]"
-                  style="width: 48px; height: 48px"
-                />
-              </RouterLink>
-            </div>
-            <p class="text-body-color dark:text-dark-6 text-base">
-              Made with ❤️ by Raúl López.
-              <br />Thanks for visiting!
-            </p>
-          </div>
-          <p class="text-dark flex items-center pt-2 text-sm dark:text-white">
-            <span>
-              This website is licensed under the GNU General Public License
-              v3.0. The source code is available in
-              <a
-                href="https://github.com/ralodev/ralodev/tree/Vuejs-v2"
-                target="_blank"
-                rel="noopener noreferrer"
-                class="border-b-2 border-blue-800 font-semibold text-blue-800 dark:border-cyan-400 dark:text-cyan-400"
-                >GitHub</a
-              >. Feel free to use it for your own projects. 🫡</span
-            >
-          </p>
-        </div>
-      </div>
-    </div>
-    <div>
-      <div
-        class="custom-shape-divider-top-1704854337 text-[#fff] dark:text-[#0c1323]"
+    <p class="text-body-color dark:text-dark-6 text-base">
+      &copy; 2024 ralodev. Some Rights Reserved.
+    </p>
+    <a
+      href="https://github.com/ralodev/ralodev/tree/Vuejs-v2"
+      target="_blank"
+      class="text-base text-cyan-800 hover:text-cyan-900 hover:underline dark:text-cyan-600 hover:dark:text-cyan-500"
+    >
+      source code
+      <svg
+        aria-hidden="true"
+        fill="currentColor"
+        viewBox="0 0 20 20"
+        class="inline h-4 w-4 pb-0.5"
       >
-        <svg
-          data-name="Layer 1"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 1200 120"
-          preserveAspectRatio="none"
-          fill="currentColor"
-        >
-          <path
-            d="M0,0V7.23C0,65.52,268.63,112.77,600,112.77S1200,65.52,1200,7.23V0Z"
-            class="shape-fill"
-          ></path>
-        </svg>
-      </div>
-    </div>
+        <path
+          fill-rule="evenodd"
+          d="M10 .333A9.911 9.911 0 0 0 6.866 19.65c.5.092.678-.215.678-.477 0-.237-.01-1.017-.014-1.845-2.757.6-3.338-1.169-3.338-1.169a2.627 2.627 0 0 0-1.1-1.451c-.9-.615.07-.6.07-.6a2.084 2.084 0 0 1 1.518 1.021 2.11 2.11 0 0 0 2.884.823c.044-.503.268-.973.63-1.325-2.2-.25-4.516-1.1-4.516-4.9A3.832 3.832 0 0 1 4.7 7.068a3.56 3.56 0 0 1 .095-2.623s.832-.266 2.726 1.016a9.409 9.409 0 0 1 4.962 0c1.89-1.282 2.717-1.016 2.717-1.016.366.83.402 1.768.1 2.623a3.827 3.827 0 0 1 1.02 2.659c0 3.807-2.319 4.644-4.525 4.889a2.366 2.366 0 0 1 .673 1.834c0 1.326-.012 2.394-.012 2.72 0 .263.18.572.681.475A9.911 9.911 0 0 0 10 .333Z"
+          clip-rule="evenodd"
+        />
+      </svg>
+    </a>
   </footer>
   <!-- ====== Footer Section End -->
 </template>
